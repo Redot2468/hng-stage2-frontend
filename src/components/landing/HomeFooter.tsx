24d3585ce528bf7fa3ba756a6831e3e0e67ca25d
Redot2468 <lawalridwan860@gmail.com>
2025-10-26 12:40:18 +0100

    fix: Change the layout of the footer

--- src/components/landing/HomeFooter.tsx
+++ src/components/landing/HomeFooter.tsx
@@ -1,6 +1,6 @@
 export default function HomeFooter() {
   return (
-    <footer className="border-t w-full py-6 px-8 space-y-5 flex justify-between items-center    ">
+    <footer className="border-t w-full py-6 px-8 space-y-5 flex justify-between items-center flex-col sm:flex-row   ">
       <div className="flex items-center gap-4">
         <img src="/logo-1.png" alt="" className="size-[30px]" />
         <h1 className="text-xl text-neutral-800 font-semibold">TicketFlow</h1>
